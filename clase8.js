cc50c088ca86324588b0f67e2c8c7c1df4685926
Victor Rolack <victorolack@gmail.com>
2020-11-09 19:46:22 -0300

    Clase 8: Parametros por valor o referencia

---
+++ clase8.js
@@ -0,0 +1,44 @@
+var victor = {
+    nombre: 'Victor',
+    apellido: 'Rolack',
+    edad: 21
+};
+
+var Claudia = {
+    nombre: 'Claudia',
+    apellido: 'Rolack',
+    edad: 29
+}
+
+
+function imprimirNombreMayus(persona) {
+    //var nombre = persona.nombre es lo mismo
+    var {nombre} = persona;
+       console.log(persona.nombre.toUpperCase());
+}
+
+/*function imprimirNombreMayus({ nombre }) {
+    console.log(nombre.toUpperCase());
+}*/
+
+imprimirNombreMayus(victor);
+imprimirNombreMayus(Claudia);
+
+//reto
+
+function imprimirNombreEdad(persona) {
+    console.log(`Hola. soy ${persona.nombre} y tengo ${persona.edad} anos`)
+}
+
+imprimirNombreEdad(victor);
+imprimirNombreEdad(Claudia);
+
+//----------
+
+function cumpleanos(persona) {
+    persona.edad += 1;
+    return {
+        ...persona, 
+        edad: persona.edad + 1
+    };
+} //los objetos se pasan por referencia. aqui te retorna un objeto nuevo.
