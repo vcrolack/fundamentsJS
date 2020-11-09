9e70c13ae2ae689b3b5d1fbf3d3b12cd71d72ddb
Victor Rolack <victorolack@gmail.com>
2020-11-08 21:48:25 -0300

    First commit and class 2

---
+++ clase2.js
@@ -0,0 +1,13 @@
+var nombre = 'Victor', apellido = 'Rolack';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length();
+
+var nombreCompleto = nombre + ' ' + apellido;
+var nombreCompleto2 = `${nombre} ${apellido.toUpperCase}`;
+
+var str = nombre.charAt(1) + nombre.charAt(2);
+var str2 = nombre.substr(1, 2); //selecciona caracteres
