52d3c64267f3f10b2fda95a1a77cf0e1a96e6d88
Victor Rolack <victorolack@gmail.com>
2020-11-12 21:55:41 -0300

    Clase 16: switch

---
+++ clase16.js
@@ -0,0 +1,13 @@
+var signo = prompt('Cual es tu signo?');
+
+console.log(`signo = ${signo}`);
+
+switch(signo) {
+    case 'acuario':
+        console.log('Deja de creer tonterias');
+        break
+    case 'escorpion':
+        console.log('Deja de creer tonterias');
+        break;
+    ...
+}
