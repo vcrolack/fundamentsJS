c32c820af2315129731f6a52f836b458367bbd2e
Victor Rolack <victorolack@gmail.com>
2020-11-27 14:04:50 -0300

    Clase 18: filtrados de arrays

---
+++ clase18.js
@@ -0,0 +1,50 @@
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    altura: 1.72
+}
+
+var alan = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.86
+}
+
+var martin = {
+    nombre: 'Martin',
+    apellido: 'Gomez',
+    altura: 1.85
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Juarez',
+    altura: 1.71
+}
+
+var vicky = {
+    nombre: 'Vicky',
+    apellido: 'Zapata',
+    altura: 1.56
+}
+
+var paula = {
+    nombre: 'Paula',
+    apellido: 'Barros',
+    altura: 1.76
+}
+
+const esAlta = persona => persona.altura > 1.8;
+const esBaja = persona => persona.altura < 1.8;
+
+
+var personas = [sacha, martin, dario, vicky, paula];
+
+var personasAltas = personas.filter(esAlta);
+var personasBajas = personas.filter(esBaja);
+//var personasAltas = personas.filter(function (persona) {
+  //  return persona.altura > 1.8;
+//})
+
+console.log(personasAltas);
+console.log(personasBajas);
